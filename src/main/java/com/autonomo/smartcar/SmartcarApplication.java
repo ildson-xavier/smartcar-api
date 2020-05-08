b5c02b49df08dd6455e8cba71a9ea0d3fb5c8989
Ildson <ildson.xavier@gmail.com>
2020-05-08 18:46:01 -0300

    Removido o tomacat embebed

--- src/main/java/com/autonomo/smartcar/SmartcarApplication.java
+++ src/main/java/com/autonomo/smartcar/SmartcarApplication.java
@@ -2,15 +2,22 @@ package com.autonomo.smartcar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class SmartcarApplication {
+public class SmartcarApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartcarApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmartcarApplication.class);
 	}
 
 }
