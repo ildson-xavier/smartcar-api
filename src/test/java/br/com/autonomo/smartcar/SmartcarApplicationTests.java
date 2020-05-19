2f751f16a2a6ba103e59d3030703f1c770512e5f
Ildson Moraes <ildsonmoraes@MacBook-Air-de-Ildson.local>
2020-05-19 20:38:22 -0300

    Atualizacao

--- src/test/java/br/com/autonomo/smartcar/SmartcarApplicationTests.java
+++ src/test/java/br/com/autonomo/smartcar/SmartcarApplicationTests.java
@@ -1,16 +1,41 @@
 package br.com.autonomo.smartcar;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import com.autonomo.smartcar.controller.HelloController;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest
+@SpringBootConfiguration
 public class SmartcarApplicationTests {
 
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	private HelloController helloController;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		mockMvc = MockMvcBuilders.standaloneSetup(helloController).build();
+	}
+
 	@Test
-	public void contextLoads() {
+	public void hello() throws Exception {
+		mockMvc.perform(get("/").accept(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().string("Hello Jenkins 2"));
 	}
 
 }
